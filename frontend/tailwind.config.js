a9eb7179de4c5853b48e36df651d97b78a344832
DhirajIngale <idhiraj2204@gmai.com>
2025-01-26 14:48:24 +0530

    quote

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fill: {
+          '0%': { width: '0%' },
+          '100%': { width: '75%' },
+        },
+      },
+      animation: {
+        fill: 'fill 2s ease-in-out forwards',
+      },
+    },
   },
   plugins: [],
-}
+};
