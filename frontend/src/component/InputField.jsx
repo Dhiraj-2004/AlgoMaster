392e607c6fa2391e2cb0b227634f7b833c72feba
DhirajIngale <idhiraj2204@gmai.com>
2025-01-28 18:25:24 +0530

    password eye icon

--- frontend/src/component/InputField.jsx
+++ frontend/src/component/InputField.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
-const InputField = ({ type, value, onChange, placeholder, label}) => {
+const InputField = ({ type, value, onChange, placeholder, label }) => {
   const { theme } = useContext(ThemeContext);
   const [themes, setThemes] = useState(theme);
+  const [showPassword, setShowPassword] = useState(false);
+
   useEffect(() => {
     setThemes(theme);
   }, [theme]);
@@ -13,20 +16,32 @@ const InputField = ({ type, value, onChange, placeholder, label}) => {
     ? 'border-gray-600 bg-zinc-800 text-white'
     : 'border-gray-300 bg-white text-black';
 
-  const labelStyles = themes === 'dark'
-    ? 'text-white'
-    : 'text-gray-900';
+  const labelStyles = themes === 'dark' ? 'text-white' : 'text-gray-900';
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="flex flex-col space-y-2 w-full">
-      {label && <label className={`text-sm font-medium ${labelStyles} text-sm font-medium`}>{label}</label>}
+    <div className="flex flex-col space-y-2 w-full relative">
+      {label && <label className={`text-sm font-medium ${labelStyles}`}>{label}</label>}
       <input
-        className={`w-full h-14 p-4 border ${inputStyles} rounded-xl focus:ring-2 focus:ring-[#ff5757] focus:outline-none`}
-        type={type}
+        className={`w-full h-14 p-4 border ${inputStyles} rounded-xl focus:ring-2 focus:ring-[#ff5757] focus:outline-none pr-12`}
+        type={type === 'password' && showPassword ? 'text' : type}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
         required
       />
+      {type === 'password' && (
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-4 top-1/2 -translate-y-1/4 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none"
+          >
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </button>
+      )}
     </div>
   );
 };
