4ed7936336de5ad6fa5f3da8f7919cd8ef7ecceb
MaheshPokake99 <pokalemahesh97@gmail.com>
2025-01-28 22:32:00 +0530

    nav

--- frontend/src/component/ThemeToggle.jsx
+++ frontend/src/component/ThemeToggle.jsx
@@ -12,7 +12,7 @@ const ThemeToggle = () => {
       onClick={toggleTheme}
       className="text-3xl transition-transform duration-1000 focus:outline-none bg-zinc-300 dark:bg-zinc-600 rounded-full h-10 w-10"
     >
-      {theme === "light" ? <FaRegMoon className="ml-1 font-bold transition-colors duration-500 " /> : <GoSun className="ml-1 text-yellow-400 font-bold transition-colors duration-500"/>}
+      {theme === "light" ? <FaRegMoon className="ml-2 font-bold transition-colors duration-500 " /> : <GoSun className="ml-2 text-yellow-400 font-bold transition-colors duration-500"/>}
     </button>
   );
 };
