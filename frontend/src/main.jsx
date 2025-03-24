e262e4055b85fdda614fa267475d2a059c981b1e
MaheshPokake99 <pokalemahesh97@gmail.com>
2025-03-24 19:31:52 +0530

    vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import App from './App.jsx'
 import './index.css'
 import "./styles/index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
         <App />
+        <Analytics />
     </AuthProvider>
 )
